cb2ccd6a63c08b3bf1be3e4af6fa3c7590578d4a
Newerdim <newerdim@onet.pl>
2020-09-26 10:39:04 +0200

    Source and functions

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-// -------- Zmienne i stałe -------- //
+// Przyda się dodatek żeby '#region folding for VS Code' żeby zwijać niepotrzebne tematy
+// Żeby zwinąć wszystkie naciśnij ctrl + shift + p i wpisz Collapse all #regions.
+
+/* #region Zmienne i stałe */
 
 /* 
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/let
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Polecenia/const
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/var
+
     Zmienna tworzymy:
         typ nazwa = wartość
 
@@ -24,9 +31,13 @@ let zmiennaLokalna = 'Polecam używać.';
 const stala = 3.14;
 // stala = 14; próba przypisanania nowej wartości skończy się błedem
 
-// -------- Tablice -------- //
+/* #endregion */
+
+/* #region Tablice */
 
 /* 
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Array
+
     Składnia:
         typ nazwa = [wartość1, wartość2, wartość3];
 */
@@ -71,7 +82,16 @@ console.log(indeks); // 2 bo tablica zaczyna się od 0
 // tablica.splice(indeks, ilośćElementow);
 var usunietyElement = tablicaLiczb.splice(indeks, 1);
 
-// -------- Okna dialogowe i konsola -------- //
+/* #endregion */
+
+/* #region Okna dialogowe i konsola */
+
+/* 
+    https://developer.mozilla.org/pl/docs/Web/API/Console/log
+    https://developer.mozilla.org/pl/docs/Web/API/Window/prompt
+    https://developer.mozilla.org/pl/docs/Web/API/Window/confirm
+    https://developer.mozilla.org/pl/docs/Web/API/Window/alert
+*/
 
 // Wyświetlenie objektu, zmiennej lub tekstu w konsoli (F12 w przeglądarce)
 // console.log(objekt);
@@ -90,9 +110,13 @@ console.log(kontynuuj); // True lub False zależy co zostało wciśnięte
 // Okienko ostrzegawcze. Nic nie zwraca
 // alert(wiadomość);
 alert('Uwaga');
+/* #endregion */
 
-// -------- Operatory arytmetyczne -------- //
+/* #region Operatory arytmetyczne */
+
 /*
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Operatory/Operatory_arytmetyczne
+
     Streszczenie:
         przypisanie: =
         dodawnie: +
@@ -175,9 +199,13 @@ liczba = 10;
 liczba %= 5;
 console.log(liczba); // 0
 
-// -------- Operatory logiczne -------- //
+/* #endregion */
+
+/* #region Operatory logiczne */
 
 /* 
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Guide/Obsolete_Pages/Przewodnik_po_j%C4%99zyku_JavaScript_1.5/Operatory/Operatory_logiczne
+
     Streszczenie: 
         lub: ||
         i: &&
@@ -212,9 +240,13 @@ console.log(10 != 5); // true
 // inny typ niż
 console.log(10 !== 10); // false
 
-// -------- If i Else -------- //
+/* #endregion */
+
+/* #region If i Else */
 
 /* 
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Polecenia/if...else
+
     Składnia:
         if (warunek) { 
             jeśli warunek jest prawdziwy to zrób to 
@@ -229,14 +261,31 @@ liczba = 10;
 
 if (liczba > 0) {
     console.log('Liczba jest większa niż 0')
-} else if(liczba < 0) {
+} else if (liczba < 0) {
     console.log('Liczba jest mniejsza niż 0');
 } else {
     console.log('Liczba nie jest większa ani mniejsza od zera');
 }
 
-// -------- Switch -------- //
+/* #endregion */
+
+/* #region Skrócony if else */
+
 /* 
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Operatory/Operator_warunkowy
+
+    Składnia:
+        warunek ? jeśli prawda to : jeśli fałsz to
+*/
+liczba = 5;
+console.log(liczba >= 0 ? 'Większa lub równa zero' : 'Mniejsza niż zero'); // Większa lub równa zero
+/* #endregion */
+
+/* #region  Switch */
+
+/* 
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Polecenia/switch
+
     Składnia:
         switch(wartość) {
             case x:
@@ -250,8 +299,8 @@ if (liczba > 0) {
         }
 */
 
-switch(liczba) {
-    case 10: 
+switch (liczba) {
+    case 10:
         console.log('Liczba jest równa 10');
         break;
     case 15:
@@ -261,8 +310,18 @@ switch(liczba) {
         console.log('Liczba nie jest ani 10 ani 15');
 }
 
-// -------- Pętle -------- //
+/* #endregion */
+
+/* #region Pętle */
+
 /* 
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Polecenia/while
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Polecenia/do...while
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Polecenia/for
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Polecenia/for...in
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Reference/Statements/for...of
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Array/forEach
+
     Rodzaje:
         while,
         do while,
@@ -280,7 +339,7 @@ switch(liczba) {
 
 liczba = 10
 
-while(liczba < 15) {
+while (liczba < 15) {
     liczba++;
 }
 
@@ -298,7 +357,7 @@ liczba = 10
 
 do {
     liczba++;
-} while(liczba > 15);
+} while (liczba > 15);
 
 console.log(liczba); // 16
 
@@ -333,15 +392,84 @@ tablica.forEach(element => {
     console.log(element); // Wypisze liczby z tablicy po kolei: 1, 4, 6, 7, 10
 })
 
-// -------- Funkcje -------- //
+/* #endregion */
 
-// -------- Funkcje strzałkowe -------- //
+/* #region Funkcje */
 
-// -------- Obiekty -------- //
+/* 
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Function
+
+    Funkcja - to pracownik który wykonuje wyznacząną prace np. liczy sumę 2 liczb 
+    Składnia:
+        Funkcja bez parametru
+        function Nazwa() {
+            kod
+        }
+
+        Funkcja z parametrem
+        function Nazwa(parametr) {
+            kod
+        }
+
+        Funkcja zwracająca wartość
+        function Nazwa() {
+            return zwracanaWartość
+        }
+*/
+function hello() {
+    console.log('Witaj');
+}
+
+hello(); // Wywołanie funkcji, pojawi się 'Witaj' w konsoli
+
+function wyswietlWynikDodawania(a, b) {
+    console.log(a + b);
+}
+
+wyswietlWynikDodawania(3, 5); // W konsoli pojawi się 8
+
+function suma(a, b) {
+    return a + b;
+}
+
+console.log(suma(3, 5)); // 8
+
+/* #endregion */
+
+/* #region Funkcje strzałkowe */
+/* 
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Reference/Functions/Funkcje_strzalkowe
+
+    Działają tak samo jak zwykłe funkcje ale różnią się budową. 
+    Można je przypisać do zmiennej
+    * parametry są opcjonalne *
+    Składnia: 
+        (parametry) => kod
+        (parametry) => {
+            kod
+        }
+*/
+
+let helloStrzalka = () => console.log('Witaj');
+helloStrzalka(); // Wywołanie funkcji, pojawi się 'Witaj' w konsoli
+
+let wyswietlWynikDodawaniaStrzalka = (a, b) => console.log(a + b);
+wyswietlWynikDodawaniaStrzalka(3, 5); // W konsoli pojawi sie 8
+
+let sumaStrzlka = (a, b) => a + b;
+console.log(sumaStrzalka(3, 5)); // 8
+/* #endregion */
+
+/* #region Obiekty */
+
+// https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Object
 
 // Tworzenie obiektu
 // typ nazwa = { pole: wartość };
-let osoba = { imie: 'Jan', wiek: 18 };
+let osoba = {
+    imie: 'Jan',
+    wiek: 18
+};
 
 // Pobranie wartości
 // objekt.pole
@@ -358,10 +486,11 @@ console.log(osoba.imie); // Janusz
 osoba.wzrost = 190;
 console.log(osoba.wzrost); // 190
 
-// -------- Dodatkowe funkcje na tablicach i objektach-------- //
+/* #endregion */
 
-let zadania = [
-    {
+/* #region Dodatkowe funkcje na tablicach i objektach */
+
+let zadania = [{
         'nazwa': 'Zadanie 1',
         'ukonczone': false
     },
@@ -375,8 +504,10 @@ let zadania = [
     },
 ];
 
-// Mapowanie - pozwala na mapowanie pól z których powstanie nowa tablica/objekt
+// Mapowanie - pozwala na mapowanie/wybieranie pól z których powstanie nowa tablica/objekt
 /*
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Array/map
+
     Składani:
         * indeks i tablica są opcjonalne *
 
@@ -391,13 +522,72 @@ let zadania = [
             kod ktory wybiera pola
             np. return element.pole
         })
+
+                lub 
+
+        tablica/objekt.filter((element, indeks, tablica) => kod ktory wybiera pola;
 */
 
 // Zwraca nową tablice tylko z nazwami zadań
 let nazwyZadania = zadania.map(zadanie => zadanie.nazwa);
 console.log(nazwyZadania); // ['Zadanie 1', 'Zadanie 2', 'Zadanie 3'];
 
-// filter reduce 
+// Filtrowanie - pozwala na wybieranie elementów i tworzenie z nich nowej tablicy/obiektu
+/*
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Array/filter
+
+    Składani:
+        * indeks i tablica są opcjonalne *
+
+        tablica/objekt.filter(function(element, indeks, tablica) {
+            kod sprawdzający czy element pasuje
+            np. return element > 10;
+        })
+
+        lub
+
+        tablica/objekt.filter((element, indeks, tablica) => {
+            kod sprawdzający czy element pasuje
+            np. return element > 10;
+        })
+
+        lub 
+
+        tablica/objekt.filter((element, indeks, tablica) => kod sprawdzający czy element pasuje;
+*/
+
+// Zwraca nową tablice tylko z zadaniami wykonanymi
+let wykonaneZadania = zadania.filter(zadanie => zadanie.ukonczone);
+console.log(wykonaneZadania); // [{'nazwa': 'Zadanie 3', 'ukonczone': true}];
+
+// Reduce - transformuje tablice/obiekt do jednej wartości np. liczenie sumy liczb
+/*
+    https://developer.mozilla.org/pl/docs/Web/JavaScript/Referencje/Obiekty/Array/Reduce
+
+    Składani:
+        * indeks i tablica są opcjonalne *
+
+        tablica/objekt.reduce(function(poprzedniaWartość, aktualnaWartość, index, tablica) {
+            transformacja
+            np. return poprzedniaWartość + aktualnaWartość;
+        })
+
+        lub
+
+        tablica/objekt.reduce((element, indeks, tablica) => {
+            transformacja
+            np. return poprzedniaWartość + aktualnaWartość;
+        })
+
+        lub 
+
+        tablica/objekt.filter((element, indeks, tablica) => poprzedniaWartość + aktualnaWartość;
+*/
+
+// Zwraca nową tablice tylko z zadaniami wykonanymi
+let sumaLiczb = [0, 1, 2, 3, 4].reduce((poprzednia, aktualna) => poprzednia + aktualna);
+console.log(sumaLiczb); // 10
+/* #endregion */
 
 // -------- Klasy -------- //
 
